fe4ff6e56fe65c70f12b58b0246b0f5027a60322
Isaiah Branstetter <isaiah.bran@icloud.com>
2024-02-07 21:42:32 -0500

    Assessment Test

---
+++ solutions.js/solutions.js
@@ -0,0 +1,12 @@
+//What is the diffrence between a parameter and an argument\\
+    // 1. A paramater is a variable in a function or method.
+    // 2. An argument is the actual value that is passed to a function or method when it is called.
+//Within a function, what is the difference between return and console.log\\
+    // 1. Return is used to specify the value that a function should output or produce.
+    // 2. Console.log is used for logging or printing info.
+
+
+
+
+    //PALINDROME\\
+    
