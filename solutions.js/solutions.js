13acc701ddd06dda26994cccd1b72f6e0c03c941
Isaiah Branstetter <isaiah.bran@icloud.com>
2024-02-07 21:57:18 -0500

    Sum Array Complete

--- solutions.js/solutions.js
+++ solutions.js/solutions.js
@@ -1,7 +1,10 @@
 //What is the diffrence between a parameter and an argument\\
+    
     // 1. A paramater is a variable in a function or method.
     // 2. An argument is the actual value that is passed to a function or method when it is called.
-//Within a function, what is the difference between return and console.log\\
+
+    //Within a function, what is the difference between return and console.log\\
+    
     // 1. Return is used to specify the value that a function should output or produce.
     // 2. Console.log is used for logging or printing info.
 
@@ -17,3 +20,20 @@
     const palindrome = (string) => string.toLowerCase() === string.toLowerCase().split('').reverse().join('') ? true:false
     console.log(palindrome("racecare"))
     
+   
+   
+    //SUM ARRAY\\
+ //Initialize a variable to store the sum of the array elements.
+//Use a for loop to iterate through the array.
+//In each iteration, add the current element to the sum variable.
+//After the loop, return the sum variable.
+//Commit your work with the message "sum array complete
+
+    function sumArray(num) {
+        let sum = 0
+        for(let i = 0; i < num.length; i++) {
+            sum += num[1]
+        }
+        return sum
+    }
+        console.log(sumArray([5,6,7,8]))
